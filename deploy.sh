0f52323181b6308cd55b282628147183efd070c3
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2019-09-26 11:38:12 +0200

    Added build script & host-repo as submodule

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Stop on fail
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+./openring.sh
+hugo -F
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
