3156dd49a66481432515cae3d9f16d6e934e2f57
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2020-03-13 00:48:01 +0100

    Update feeds

--- openring.sh
+++ openring.sh
@@ -1,7 +1,8 @@
 openring \
   -s https://artificial-mind.net/feed.xml \
   -s https://www.msoos.org/feed \
-  -s https://blog.regehr.org/feed \
+  -s https://deepnight.net/feed \
   -s https://zipcpu.com/feed.xml \
+  -s http://fabiensanglard.net/rss.xml \
   < openring-template.toml \
   > data/webring.toml
