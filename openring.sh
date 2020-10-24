a868874e8468a0b6f1736807af9d98b9a32cf580
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2020-10-24 18:58:45 +0200

    Update blog feeds

--- openring.sh
+++ openring.sh
@@ -4,5 +4,7 @@ openring \
   -s https://deepnight.net/feed \
   -s https://zipcpu.com/feed.xml \
   -s http://fabiensanglard.net/rss.xml \
+  -s https://secret.club/feed.xml \
+  -s https://softologyblog.wordpress.com/feed.xml \
   < openring-template.toml \
   > data/webring.toml
